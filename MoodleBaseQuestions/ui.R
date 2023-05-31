afe2a6f950109ab89e350edf11226cc1b8c026e2
Marie Verbanck <68858208+marieverbanck@users.noreply.github.com>
2023-05-31 22:00:32 +0200

    Nettoyage ui.R

--- MoodleBaseQuestions/ui.R
+++ MoodleBaseQuestions/ui.R
@@ -32,17 +32,14 @@ mytheme <- create_theme(
 
 shinyUI(
 
-  
-  
   #####################################################################################
   
-
   dashboardPage(skin = "purple",
 	dashboardHeader(
 		title = "SARP moodle",
 		tags$li(class = "dropdown",
 		        tags$a(href = "#", class = "dropdown-toggle", `data-toggle` = "dropdown",
-		               tags$img(src = "Logo BioSTM.png", height = "60px", width = "80px"))
+		               tags$img(src = "Logo-BioSTM.png", height = "80px", width = "80px"))
 		       
 		),
 		dropdownMenuOutput("messageMenu")
@@ -79,54 +76,50 @@ shinyUI(
 ############################# Body ##############################
 
 	dashboardBody(
-	  fluidRow(
-	    column(width = 3,
-	           img(src = "UniversiteParisCite_Pharmacie.jpeg", height = "157px", width = "500px")
-	    )
-	  ),
-  
 	  use_theme(mytheme),
 		tabItems(
 			tabItem(tabName = "Base",
 
 				################ Premiere partie introductive
-				linebreaks(3),
-  				fluidRow(
-  				  column(width = 6, align = "center",
-  				         imageOutput("picture")
-  				  ),
-  			
+				fluidRow(
+				  img(src = "UniversiteParisCite_Pharmacie.jpeg", height = "157px", width = "500px")
+				),
+				linebreaks(1),
+				fluidRow(
 					valueBox("SARP moodle", "Bienvenue dans l'assistant SARP moodle. Cet assistant vous permet à partir d'un simple fichier Excel, Ods, ou Csv de récupérer un fichier XML que vous pourrez importer sur moodle pour en faire une base de questions.", 
 						icon = icon("info"),
 						color = "purple", 
-						width = 10
-  					)
-  				),
-  				fluidRow(	
-  					infoBox("En entrée : un fichier excel", "Préparez votre fichier (xlsx, ods, csv) comportant toutes les questions de votre future base selon le modèle.", 
+						width = 12
+  				)
+  			),
+  			fluidRow(	
+  				infoBox("En entrée : un fichier excel", "Préparez votre fichier (xlsx, ods, csv) comportant toutes les questions de votre future base selon le modèle.", 
 						icon = icon("file-excel"),
 						color = "purple", 
-						width = 10
-  					),
-						infoBox("En entrée : des fichiers d'images", "Préparez tous vos fichiers images (jpeg, jpg, png) utilisées dans votre base de question.", 
+						width = 12
+  				),
+					infoBox("En entrée : des fichiers d'images", "Préparez tous vos fichiers images (jpeg, jpg, png) utilisées dans votre base de question.", 
 						icon = icon("images"),
 						color = "purple", 
-						width = 10
-						),
-  					align = "center"
-  				),
-  				fluidRow(
-  				  infoBox("En sortie", "Vous pourrez ensuite télécharger un fichier XML qui peut être directement importé dans Moodle.", 
+						width = 12
+					),
+  			  infoBox("En sortie", "Vous pourrez ensuite télécharger un fichier XML qui peut être directement importé dans Moodle.", 
   				  icon = icon("graduation-cap"),
   				  color = "purple", 
-  				  width = 10
-  				  ),
-  					valueBox("",
+  				  width = 12
+  			  ),
+  			  align = "center"
+  			),
+				fluidRow(
+				  column(3),
+				  column(6, align = "center",
+  				valueBox("",
 						actionButton("Start", "Commencer"),
 						color = "black",
-						width = 3
-  					),
-  					align = "center"
+						width = 12
+  				),
+  				column(3),
+				  )
 				)
 			),
   			tabItem(tabName = "Conversion",
