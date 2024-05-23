54f96a0df25f518bfe70e9405284f94032341921
Sabrina <114215692+Saabb9@users.noreply.github.com>
2024-05-23 16:45:59 +0200

    23/05/24
    selection images multiples + cocher images qu'on souhaite garder + mess doublons ( a retravailler)

--- selecteur_imagesMultiples.R
+++ selecteur_imagesMultiples.R
@@ -4,53 +4,93 @@ ui <- fluidPage(
   titlePanel("Sélection multiple d'images"),
   sidebarLayout(
     sidebarPanel(
+      fileInput("images", label = "Sélectionnez des images", multiple = TRUE, accept = c('image/png', 'image/jpeg', 'image/jpg')),
       hr(),
       uiOutput("image_selectors")
     ),
     mainPanel(
       h4("Images sélectionnées :"),
-      uiOutput("selected_images")
+      uiOutput("selected_images"),
+      actionButton("validate_images", "Valider les images")
     )
   )
 )
 
 server <- function(input, output, session) {
-  image_count <- reactiveVal(0)
+  
   selected_images <- reactiveVal(list())
   
-  observeEvent(input$add_image, {
-    image_count(image_count() + 1)
+  observeEvent(input$images, {
+    # Si on observe un nouvel usage de "fileInput"
+    images <- selected_images() # Recuperation des images importees jusque la
+    new_images <- lapply(seq_along(input$images$name), function(i) { # lapply: permet de faire une boucle for
+      list(name = input$images$name[i], datapath = input$images$datapath[i]) # input$images$datapath: chemin de l'image dans l'application
+    })
+    # for (i in seq_along(input$images$name)){ # seq_along(input$images$name) un vecteur avec les indices des images selectionnees 1:nbImages. Autre facon de le faire 1:length(input$images$name)
+    #    list( # on cree une liste avec 2 elements 
+    #        name = input$images$name[i], # 1er element nom de l'image i
+    #        datapath = input$images$datapath[i] # 2eme element chemin de l'image i
+    #   )
+    # }
+    names(new_images) <- input$images$name  # renommer les elements de la liste avec les noms des images
+    # Vérification des doublons
+    duplicate_names <- names(selected_images())[names(selected_images()) %in% input$images$name]
+    if (length(duplicate_names) > 0) {
+      showModal(modalDialog(
+        title = "Attention",
+        paste("Les images suivantes ont été sélectionnées plusieurs fois :", paste(duplicate_names, collapse = ", ")),
+        footer = NULL
+      ))
+    } else {
+    images <- c(images, new_images) # concatene anciennes images et nouvelles. images est une liste de taille egale aux nombres d'images importees depuis le debut (anciennes + nouvelles) et avec chaque element qui est une liste de 2 elements name et datapath.
+    selected_images(images) # Mise a jour de la liste d'images
+    }
   })
   
-  output$image_selectors <- renderUI({
-    fileInput("images", label = "", multiple = TRUE, accept = c('image/png', 'image/jpeg', 'image/jpg'))
+  output$selected_images <- renderUI({ # Renvoit un element du UI (user interface)
+    img_list <- selected_images() # Recuperation des images importees jusque la
+    tagList(
+      lapply(names(img_list), function(name) {# Une boucle for pour chaque element de names(img_list), donc pour chaque nom d'image
+        tags$div(
+          checkboxInput(inputId = paste0("select_", name), label = "", value = TRUE),
+          h5(name),
+          tags$hr()
+        )
+      })
+    )
+  })
+  
+  observeEvent(input$validate_images, {
+    selected <- reactiveVal(NULL) # Crée une reactiveVal pour stocker les images sélectionnées
+    observe({
+      # Liste des valeurs de toutes les cases à cocher correspondant à chaque image sélectionnée
+      input_list <- lapply(names(selected_images()), function(name) input[[paste0("select_", name)]])
+      # Sélectionne les noms des images pour lesquelles les cases à cocher sont cochées
+      selected(names(selected_images())[unlist(input_list)])
+    })
+    # Affiche une boîte de dialogue modale contenant les images sélectionnées
+    showModal(modalDialog(
+      title = "Images validées",
+      renderText({
+        # Affiche les noms des images sélectionnées dans un texte
+        paste("Voici les images que vous avez sélectionnées :", paste(selected(), collapse = ", "))
+      }),
+      footer = NULL,
+      easyClose = T
+    ))
   })
   
   observe({
-    observeEvent(input$images, {
-      images <- selected_images()
-      images[[input$images$name]] <- input$images
-      selected_images(images)
-    })
-  })
-  
-  output$selected_images <- renderUI({
-    img_list <- selected_images()
-    tagList(
-      lapply(names(img_list), function(name) {
-        imgs <- img_list[[name]]
-        if (!is.null(imgs)) {
-          tags$div(
-            h5(name),
-            tags$ul(
-              lapply(imgs$name, function(fname) {
-                tags$li(fname)
-              })
-            )
-          )
-        }
+    img_list <- selected_images() # Recuperation des images importees jusque la 
+    lapply(names(img_list), function(name) { # Boucle for pour chaque image
+      observeEvent(input[[paste0("delete_", name, "_btn")]], {
+        images <- selected_images()
+        images[[name]] <- NULL
+        # imagesUpdated <- images[names(images) != name]
+        # Probleme pour reimporter une image deja supprimee ds input[[paste0("delete_", name, "_btn")]]
+        selected_images(images)
       })
-    )
+    })
   })
 }
 
